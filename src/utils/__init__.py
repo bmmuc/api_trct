9483d381c0282ed46fb7a27fb49af744f2d20019
bmmuc <bmmuc@cin.ufpe.br>
2025-11-02 22:10:57 -0300

    ref: removed unsued code and update utils

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -2,6 +2,16 @@
 Utilities package.
 """
 
-from .metrics import MetricsTracker
+from src.utils.base_metrics import BaseMetricsExporter
+from src.utils.memory_metrics import MemoryMetricsExporter
+from src.utils.logger import logger
+from src.utils.prometheus_metrics import PrometheusMetricsExporter
+from src.utils.metrics_factory import MetricsFactory
 
-__all__ = ["MetricsTracker"]
+__all__ = [
+    "BaseMetricsExporter",
+    "MemoryMetricsExporter",
+    "PrometheusMetricsExporter",
+    "MetricsFactory",
+    "logger",
+]
